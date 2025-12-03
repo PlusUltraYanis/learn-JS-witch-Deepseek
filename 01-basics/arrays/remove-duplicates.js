f74f96e6bcb8e1342063c4c6983b2d1a37ad23f1
YanYan <blackhawkru@mail.ru>
2025-12-03 18:18:39 +0300

    solver remove-duplicetes.js

--- 01-basics/arrays/remove-duplicates.js
+++ 01-basics/arrays/remove-duplicates.js
@@ -1,5 +1,18 @@
 "use strict"
-
+// 1. Через filter и indexOf
 function removeDuplicates(arr) {
     return arr.filter((el, index) => arr.indexOf(el) === index);
+}
+
+// 2. Через Set
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+// 3. Через reduce
+function removeDuplicates(arr) {
+    return arr.reduce((acc, el, i) => {
+        if (!acc.includes(el)) acc.push(el);
+        return acc;
+    }, [])
 }
