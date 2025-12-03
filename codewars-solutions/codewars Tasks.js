25b04e2d0ce16b187c979db05c0b9c3ffb3acfbf
YanYan <blackhawkru@mail.ru>
2025-12-03 17:37:34 +0300

    solved three tasks in "codewar Tasks"

--- codewars-solutions/codewars Tasks.js
+++ codewars-solutions/codewars Tasks.js
@@ -1,8 +1,20 @@
 "use strict"
+// tasks 1 
+// https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+// function areYouPlayingBanjo(name) {
+//   return name[0].toLowerCase() === 'r' ? `${name} plays banjo` : `${name} does not play banjo`
+// }
 
 
+// tasks 2
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+// function makeNegative(num) {
+//   return num < 0 ? num : -num;
+// }
 
 
-
-
-
+// tasks 3
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+// function greet(name){
+//   return `Hello, ${name} how are you doing today?`
+// }
